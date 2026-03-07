8b1c0cfe56c3a2bb4d652786bf00293ca3f1d59a
ansil <ansil5252@gmail.com>
2026-03-07 09:55:55 +0530

    Initial project setup

---
+++ frontend/my-react-app/src/auth/register.jsx
@@ -0,0 +1,12 @@
+function Register(){
+    return(
+        <>
+        <div>
+            <input type="text"></input>
+        </div>
+            
+        </>
+    )
+}
+
+export default Register
