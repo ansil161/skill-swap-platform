961a1c549e566c70541fd8e0b35885f9dee05555
ansil <ansil5252@gmail.com>
2026-03-12 13:00:03 +0530

    backedn swap sytem model add

--- backend/skillswap/swapsystem/models.py
+++ backend/skillswap/swapsystem/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from userprofile.models import profile
+from skills.models import skill_wanted,skilloffered
 
 # Create your models here.
+
+
+class SwapRequest(models.Model):
+    requester=models.ForeignKey(profile,on_delete=models.CASCADE,related_name='swap_sent')
+    provider=models.ForeignKey(profile,on_delete= models.CASCADE,related_name='recieved_swap')
+    skill=models.ForeignKey(skilloffered,on_delete=models.CASCADE)
+
+    status_choice=[
+        ('Pending','Pending'),
+        ('Accepted','Accepted'),
+        ('Rejected','Rejected')
+    ]
+
+    status=models.CharField(max_length=10,choices=status_choice,default='Pending')
+    create=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.requester.user.username}: {self.provider.user.username}:{self.skill.skills.name}"
+
