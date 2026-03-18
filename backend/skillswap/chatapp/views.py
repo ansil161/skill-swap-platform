5e0ecf472958721f363ccb7966cec3676ec4f288
ansil <ansil5252@gmail.com>
2026-03-18 21:35:26 +0530

    add backedn chat app consumer chages

--- backend/skillswap/chatapp/views.py
+++ backend/skillswap/chatapp/views.py
@@ -1,20 +1,30 @@
 # views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Conversation, Message
-from .serializer import MessageSerializer, ConversationSerializer
+from .models import Conversation, ChatMessage
+from .serializer import ChatMessageSerializer, ConversationSerializer
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Conversation
 
 class ConversationListView(APIView):
     def get(self, request):
-       
         user = request.user
-        conversations = Conversation.objects.filter(user1=user) | Conversation.objects.filter(user2=user)
-        serializer = ConversationSerializer(conversations, many=True)
+        
+        conversations = Conversation.objects.filter(
+            swap_request__requester=user
+        ) | Conversation.objects.filter(
+            swap_request__receiver=user
+        )
+
+     
+        serializer = ConversationSerializer(conversations.distinct(), many=True)
         return Response(serializer.data)
 
 
 class ChatMessageListView(APIView):
     def get(self, request, conversation_id):
-        messages = Message.objects.filter(conversation_id=conversation_id).order_by("timestamp")
-        serializer = MessageSerializer(messages, many=True)
+        messages = ChatMessage.objects.filter(conversation_id=conversation_id).order_by("timestamp")
+        serializer = ChatMessageSerializer(messages, many=True)
         return Response(serializer.data)
--- frontend/my-react-app/src/hooks/websocketchga.jsx
+++ frontend/my-react-app/src/hooks/websocketchga.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import api from "../../api/axios";
+import api from "../api/axios";
 
 export const useChatWebSocket = (conversationId) => {
   const socketRef = useRef(null);
