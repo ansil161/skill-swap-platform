2bb11c8e79bd055bc77618000e7d8a55dd417153
ansil <ansil5252@gmail.com>
2026-03-29 16:11:53 +0530

    add backedn ai service

--- backend/skillswap/ai_service/services/rag_pipeline.py
+++ backend/skillswap/ai_service/services/rag_pipeline.py
@@ -1,6 +1,6 @@
 from .resume_parse import resume_parse
 from .embedding import resume_embedding
-from skill.backend.skillswap.ai_service.services.chroma_store import add_vector,searchs
+from .chroma_store import add_vector,searchs
 from grok_llm import analyse_resume
 from .chunk import chunk_text
 import json
@@ -9,24 +9,27 @@ def resume_process(application):
   
     resume_converter=resume_parse(application.resume.path)
     chunks=chunk_text(resume_converter)
+    add_vector(chunks,application.id)
+    retrieve=searchs(application.job.description,
+                     application.id)
+    result=analyse_resume(
+        application.job.description,
+        retrieve
+    )
 
-    for i in chunks:
-        vector=resume_embedding(i)
-        add_vector(vector,i)
-    job_description=application.job.description
-   
-    job_vector=resume_embedding(job_description)
-    get_filter=searchs(job_vector,k=5)
-    result=analyse_resume(job_description,get_filter)
     try:
         data=json.load(result)
-    except :
+    except:
         data={
             'score':None,
-            'missing_skills':[],
-            'suggestion':None
+            'missink_skills':[],
+            'suggestion':[result]
+
         }
+
     return data
+
+   
 
 
 
