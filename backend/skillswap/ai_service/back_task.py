26964ad90cd9a3fca1a0c0add57eee9692d27b26
ansil <ansil5252@gmail.com>
2026-03-30 09:50:06 +0530

    add backedn ai service back_task

--- backend/skillswap/ai_service/back_task.py
+++ backend/skillswap/ai_service/back_task.py
@@ -1,13 +1,16 @@
 from celery import shared_task
-from .services.rag_pipeline import pr
+from .services.rag_pipeline import resume_process
+from jobs.models import JobApplication
+
+
 
 @shared_task
 def process_application_task(application_id):
-    from job.models import JobApplication
+   
 
     app = JobApplication.objects.get(id=application_id)
 
-    result = process_application(app)
+    result = resume_process(app)
 
     app.ats_score = result.get("score")
     app.ats_feedback = str(result)
