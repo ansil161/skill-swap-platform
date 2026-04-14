56863bc82f3edc4cc7bea769aa26caffa7cc2df7
ansil <ansil5252@gmail.com>
2026-04-14 17:45:57 +0530

    add the chages of chatappmidlleware

--- backend/skillswap/chatapp/middleware.py
+++ backend/skillswap/chatapp/middleware.py
@@ -4,28 +4,28 @@ from channels.middleware import BaseMiddleware
 from django.contrib.auth.models import AnonymousUser
 from userprofile.models import profile
 from asgiref.sync import sync_to_async
-
+from urllib.parse import parse_qs 
 
 class JWTAuthMiddleware(BaseMiddleware):
-
     async def __call__(self, scope, receive, send):
-        headers = dict(scope["headers"])
+        
+        query_string = scope.get("query_string", b"").decode()
+        query_params = parse_qs(query_string)
+        token = query_params.get("token", [None])[0]
 
-        cookies = headers.get(b'cookie', b'').decode()
-        token = None
-
-       
-        for cookie in cookies.split(';'):
-            if 'access_token=' in cookie:
-                token = cookie.split('=')[1].strip()
+      
+        if not token:
+            headers = dict(scope["headers"])
+            cookies = headers.get(b'cookie', b'').decode()
+            for cookie in cookies.split(';'):
+                if 'access_token=' in cookie:
+                    token = cookie.split('=')[1].strip()
 
         if token:
             try:
                 decoded = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
                 user_id = decoded.get("user_id")
-
                 scope["user"] = await self.get_user(user_id)
-
             except Exception as e:
                 print("JWT ERROR:", e)
                 scope["user"] = AnonymousUser()
@@ -37,6 +37,7 @@ class JWTAuthMiddleware(BaseMiddleware):
     @sync_to_async
     def get_user(self, user_id):
         try:
+            
             return profile.objects.get(user__id=user_id).user
         except:
             return AnonymousUser()
