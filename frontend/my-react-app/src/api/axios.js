590704fdd4b591d42798ca50b94f7f36d074b1bb
ansil <ansil5252@gmail.com>
2026-04-14 14:25:48 +0530

    add the chages of app

--- frontend/my-react-app/src/api/axios.js
+++ frontend/my-react-app/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://skillexchange.duckdns.org/skill/",
-  withCredentials: true,
+
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
