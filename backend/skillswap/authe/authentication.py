d435ec5b618a2b7ab2ced8c10eda58e6d46f8ee1
ansil <ansil5252@gmail.com>
2026-04-14 14:33:19 +0530

    add the of auth app authntication

--- backend/skillswap/authe/authentication.py
+++ backend/skillswap/authe/authentication.py
@@ -3,11 +3,18 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class CookieJWTAuthentication(JWTAuthentication):
 
     def authenticate(self, request):
-        raw_token = request.COOKIES.get("access_token")
- 
-        if raw_token is None:
-            return None
+  
+        header = self.get_header(request)
+        if header is not None:
+            raw_token = self.get_raw_token(header)
+            if raw_token:
+                validated_token = self.get_validated_token(raw_token)
+                return self.get_user(validated_token), validated_token
 
-        validated_token = self.get_validated_token(raw_token)
-        
-        return self.get_user(validated_token), validated_token
+       
+        raw_token = request.COOKIES.get("access_token")
+        if raw_token:
+            validated_token = self.get_validated_token(raw_token)
+            return self.get_user(validated_token), validated_token
+
+        return None
