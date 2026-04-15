d9dceb8885789015336b2fd494eaab59f67d1880
ansil <ansil5252@gmail.com>
2026-04-15 17:43:22 +0530

    add the chages of auservicd

--- backend/skillswap/ai_service/services/embedding.py
+++ backend/skillswap/ai_service/services/embedding.py
@@ -1,6 +1,21 @@
 from sentence_transformers import SentenceTransformer
+import os
+current_file = os.path.abspath(__file__)
+services_dir = os.path.dirname(current_file)
+ai_service_dir = os.path.dirname(services_dir)
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model_path = os.path.join(ai_service_dir, 'model_weights')
+
+
+
+if os.path.exists(model_path):
+    model = SentenceTransformer(model_path)
+    print(" Loaded model from local weights")
+else:
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+    print("Local weights not found, downloading")
+
+
 
 
 
