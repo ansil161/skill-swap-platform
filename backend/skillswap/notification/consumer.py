88965b5daf302517e76a826926f8f699a3d49d33
ansil <ansil5252@gmail.com>
2026-04-15 09:44:52 +0530

    add the changes of notification

--- backend/skillswap/notification/consumer.py
+++ backend/skillswap/notification/consumer.py
@@ -2,25 +2,17 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 class Notificationconsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
-        self.room_group_name = f"chat_{self.conversation_id}"
-        user = self.scope["user"] 
-        
-        print(f"Connecting: {self.room_group_name} - User: {user}")
-
-      
+        user=self.scope['user']
         if user.is_anonymous:
-            print(f"Chat rejection: Anonymous user tried to connect to room {self.conversation_id}")
             await self.close()
             return
-
-     
+        self.group_name=f"user_{user.id}"
         await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+            self.group_name,
+            self.channel_name,
 
-        await self.accept() 
+        )
+        await self.accept()
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.group_name,
