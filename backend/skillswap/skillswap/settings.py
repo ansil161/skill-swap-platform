53530da4b6db4b948b9950fef46e177b6c429b98
ansil <ansil5252@gmail.com>
2026-04-15 15:14:20 +0530

    add the chage of videocall page

--- backend/skillswap/skillswap/settings.py
+++ backend/skillswap/skillswap/settings.py
@@ -119,6 +119,8 @@ AWS_S3_REGION_NAME = 'eu-north-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
 CORS_ALLOW_METHODS = [
     "DELETE",
     "GET",
