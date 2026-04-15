c8a5562363a80d29fef606b78a1e09796ff5713e
ansil <ansil5252@gmail.com>
2026-04-15 09:21:40 +0530

    changes of chatapp consumer

--- backend/skillswap/notification/consumer.py
+++ backend/skillswap/notification/consumer.py
@@ -2,16 +2,25 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 class Notificationconsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user=self.scope['user']
-        if user.is_anonymous:
-            await self.close()
-        self.group_name=f"user_{user.id}"
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name,
+        self.conversation_id = self.scope['url_route']['kwargs']['conversation_id']
+        self.room_group_name = f"chat_{self.conversation_id}"
+        user = self.scope["user"] 
+        
+        print(f"Connecting: {self.room_group_name} - User: {user}")
 
+      
+        if user.is_anonymous:
+            print(f"Chat rejection: Anonymous user tried to connect to room {self.conversation_id}")
+            await self.close()
+            return
+
+     
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
         )
-        await self.accept()
+
+        await self.accept() 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.group_name,
